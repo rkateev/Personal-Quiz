ee7ae77d1101f4cf3191ae447911aadafb5d054c
rkateev <60319058+rkateev@users.noreply.github.com>
2021-07-22 07:48:59 +0300

    added array sorting

--- Personal Quiz/ViewControllers/ResultsViewController.swift
+++ Personal Quiz/ViewControllers/ResultsViewController.swift
@@ -12,20 +12,49 @@ class ResultsViewController: UIViewController {
     
     @IBOutlet weak var headLabel: UILabel!
     @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var backButton: UINavigationItem!
     
     var answers : [Answer] = []
     
-    
-    // 1. Передать сюда массив с ответами
-    // 2. Определить наиболее часто встерчающийся тип живтоного
-    // 3. Отобразить результат в соответсвии с этим животным
     // 4. Избавиться от кнопки возврата назад на экране результатов
-    
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.navigationItem.setHidesBackButton(true, animated: true)
+        let currentAnimal = countAnimals(answers)
+        headLabel.text = "Вы - \(currentAnimal!.rawValue)!"
+        textLabel.text = currentAnimal?.definition
     }
     
+}
+extension ResultsViewController {
+    private func countAnimals(_ answers: [Answer]) -> AnimalType? {
+        var arrayOfAnimals = [AnimalType]()
+        
+        for answer in answers {
+            arrayOfAnimals.append(answer.type)
+        }
+        
+        var result: AnimalType?
+        var dictionary = [AnimalType: Int]()
+            
+        arrayOfAnimals.forEach { (element) in
+            if let count = dictionary[element] {
+                dictionary[element] = count + 1
+            } else {
+                dictionary[element] = 1
+            }
+        }
+            
+        var max = 0
+            
+        for(key, value) in dictionary {
+            if value > max {
+                max = value
+                result = key
+            }
+        }
+        
+        return result
+    }
 }
